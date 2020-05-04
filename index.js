08371f161bad72484f579a6a527f9ff952d839bf
xavioo <rucahoxavi@gmail.com>
2020-05-04 11:30:02 +0200

    mongo db connection

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express=require('express');
+const mongoose=require('mongoose');
+const {mongoDbUrl} = require('./config/db');
 
 const app=express();
+
+//db connection
+mongoose.connect(mongoDbUrl,{useNewUrlParser:true, useFindAndModify: false, useCreateIndex: true}).then(db=>{
+    console.log('mongo connected');
+
+}).catch(error=>console.log('error'));
 
 
 
