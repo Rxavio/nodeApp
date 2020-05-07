cf74ee655e27de3d6ef6634b28ddf6a23bff33a5
xavioo <rucahoxavi@gmail.com>
2020-05-07 09:08:04 +0200

    add some settings and configure .env

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const {mongoDbUrl} = require('./config/db');
+const path=require('path');
+const exphbs  = require('express-handlebars');
+const bodyParser=require('body-parser');
+const dotenv=require('dotenv');
+dotenv.config();
+
 
 const app=express();
+const port = process.env.PORT;
 
 //db connection
 mongoose.connect(mongoDbUrl,{useNewUrlParser:true, useFindAndModify: false, useCreateIndex: true}).then(db=>{
@@ -11,10 +18,24 @@ mongoose.connect(mongoDbUrl,{useNewUrlParser:true, useFindAndModify: false, useC
 }).catch(error=>console.log('error'));
 
 
+app.use(express.static(path.join(__dirname,'public')));
+
+
+//set view engine
+app.engine('handlebars', exphbs({defaultLayout: 'home'}));
+app.set('view engine', 'handlebars');
+
+
+//body parser
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 
 
 
-app.listen(4500,()=>{
-    console.log('listening on port 4500');
-});
+
+
+app.listen(port, () => {
+    console.log(`Server start on port ${port}`);
+  });
+  
