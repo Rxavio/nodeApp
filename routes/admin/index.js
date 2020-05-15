bc16bc3d8ae53ceb05e5af0a732934f2d69d5772
xavioo <rucahoxavi@gmail.com>
2020-05-15 07:37:15 +0200

    count all posts,users and comments and display them on dashboard

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,5 +1,8 @@
 const express=require('express');
 const router=express.Router();
+const Post = require('../../models/Post');
+const User = require('../../models/User');
+const Comment = require('../../models/Comment');
 const {userAuthenticated} = require('../../helpers/authentication');
 
 router.all('/*',userAuthenticated,(req,res,next)=>{
@@ -7,8 +10,16 @@ router.all('/*',userAuthenticated,(req,res,next)=>{
     next();
     });
 router.get('/',(req,res)=>{
-    res.render('admin/'); 
+    const promises = [
+        Post.count().exec(),
+        User.count().exec(),
+        Comment.count().exec()
+    ];
+    Promise.all(promises).then(([postCount,userCount,commentCount])=>{
+        res.render('admin', {postCount: postCount,userCount: userCount,commentCount: commentCount});
     });
+
+});
 
 
  module.exports=router;
